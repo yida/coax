f2fc233103afa326df5582667ed168ed97c1e173
Yida Zhang <hitzhangyd@gmail.com>
2012-02-08 00:39:29 -0500

    fix size kernel

--- matlab/sym_kernel.m
+++ matlab/sym_kernel.m
@@ -6,19 +6,24 @@ file = dir(strcat('image/fly2/*.jpg'));
 nfile = size(file,1);
 
 %%
-%for i = 1 : nfile
-for cnt = 300;
+for cnt = 1 : nfile
+%for cnt = 200;
 filename = file(cnt).name;
 img = imread(strcat('image/fly2/',filename));
 img = rgb2gray(img);
 
 %%
 
-% square size
-side = 150;
-
-img = imresize(img,0.5);
+meanIMG = zeros(3,64);
+img = imresize(img,0.1);
 [imgN,imgM] = size(img);
+% square size
+
+nside = [18,24,36];
+for cnt = 1:3
+    side = nside(cnt);
+
+
 %imshow(img);
 
 Ker = ones(side);
@@ -30,19 +35,42 @@ for cnti = 1 : (imgN - side - 1)
     for cntj = 1 : (imgM - side -1)
         ker = img(cnti:cnti+side-1,cntj:cntj+side-1);
         leftpart = ker(:,1:end/2);
+
         rightpart = ker(:,end/2+1:end);
         %R = corrcoef(double(fliplr(leftpart)),double(rightpart));
         %IMG(cnti,cntj) = R(1,2);
         IMG(cnti,cntj) = abs(sum(sum(fliplr(leftpart)))-sum(sum(rightpart)));
+        %IMG(cnti,cntj) = sum(sum(abs(fliplr(leftpart)-rightpart)));
     end
 end
 
-meanIMG = mean(IMG,1);
-figure;
-subplot(2,1,1);
+%stdIMG = std(IMG,1);
+meanIMG(cnt,(side/2+1):(end-side/2)) = mean(IMG,1);
+
+end
+
+meanIMG(1,:) = meanIMG(1,:)./sum(meanIMG(1,:));
+meanIMG(2,:) = meanIMG(2,:)./sum(meanIMG(2,:));
+meanIMG(3,:) = meanIMG(3,:)./sum(meanIMG(3,:));
+
+fig = figure(1);
+subplot(5,1,1);
 imagesc(img);
-subplot(2,1,2);
-plot(1:numel(meanIMG)-1,meanIMG(1:end-1));
+colormap(gray);
+subplot(5,1,2);
+plot(1:numel(meanIMG(1,:))-1,meanIMG(1,1:end-1));
+%axis([-18/2,numel(meanIMG(1,:))+18/2,min(meanIMG(1,:)),max(meanIMG(1,:))]);
+subplot(5,1,3);
+plot(1:numel(meanIMG(2,:))-1,meanIMG(2,1:end-1));
+%axis([-24/2,numel(meanIMG(2,:))+24/2,min(meanIMG(2,:)),max(meanIMG(2,:))]);
+subplot(5,1,4);
+plot(1:numel(meanIMG(3,:))-1,meanIMG(3,1:end-1));
+%axis([-36/2,numel(meanIMG(3,:))+36/2,min(meanIMG(3,:)),max(meanIMG(3,:))]);
+subplot(5,1,5);
+plot(1:numel(meanIMG(3,:))-1,0.2*meanIMG(1,1:end-1)+0.5*meanIMG(2,1:end-1)+0.3*meanIMG(3,1:end-1));
+
+% figure;
+% surf(IMG);
 %figure;
 %image(IMG);
 %KER = IMG(1+side/2:1+side/2+imgN-side,...
@@ -63,10 +91,10 @@ plot([minRow(i),minRow(i),minRow(i)+side,minRow(i)+side,minRow(i)],...
 end
 
 toc
-
-out = 'results1/';
-print(fig,strcat(out,filename),'-djpeg','-r72');
 %}
+out = 'results5/';
+print(fig,strcat(out,filename),'-djpeg','-r72');
+
 toc;
 end
  %% draw square
