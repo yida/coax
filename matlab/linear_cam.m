5da9e785212b10b7f89a8f7d2d6a6de9b827da69
Yida Zhang <hitzhangyd@gmail.com>
2012-05-09 14:06:11 -0400

    update linear cam

--- matlab/linear_cam.m
+++ matlab/linear_cam.m
@@ -3,8 +3,9 @@ function linearImgI = linear_cam()
 %bagDir = '~/bagfiles/2011-10-06-13-56-29/';
 %bagDir = '~/bagfiles/2011-11-02-16-22-03-upstraight/';
 %bagDir = '~/bagfiles/2011-11-02-16-25-44-updrunk/';
-bagDir = '~/bagfiles/2011-11-02-16-30-06-midstraight/';
+%bagDir = '~/bagfiles/2011-11-02-16-30-06-midstraight/';
 %bagDir = '~/bagfiles/2011-11-02-16-33-24-middrunk/';
+bagDir = '~/2012-02-09-00-01-47/';
 
 imgDir = 'images/';
 imgDir = strcat(bagDir,imgDir);
@@ -17,7 +18,7 @@ img = imread(strcat(imgDir,file(306).name));
 
 for reIdx = 10
 disp(reIdx);
-linearImgI = zeros(nfile,floor(imgM*reIdx/100));
+linearImgI = zeros(nfile,imgM);
 
 t = CTimeleft(size(linearImgI, 1));
 for fileIdx = 1:nfile
@@ -26,7 +27,7 @@ fileName = file(fileIdx).name;
 
 img = imread(strcat(imgDir,fileName));
 imgI = rgb2gray(img);
-imgI = imresize(imgI,reIdx/100);
+%imgI = imresize(imgI,reIdx/100);
 [~,m] = size(imgI);
 %imagesc(imgI);
 %colormap(gray);
@@ -34,6 +35,6 @@ imgI = imresize(imgI,reIdx/100);
 linearImgI(fileIdx,1:m) = mean(imgI,1);
 end
 
-imwrite(uint8(linearImgI),strcat(bagDir,num2str(reIdx),'P',fileName));
+%imwrite(uint8(linearImgI),strcat(bagDir,num2str(reIdx),'P',fileName));
 
 end  
